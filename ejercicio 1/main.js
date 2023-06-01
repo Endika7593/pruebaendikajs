ac65b2c186fa36e84dc47d5f1fae244627bd2154
Endika7593 <e.acedo93@gmail.com>
2023-06-01 11:04:52 +0200

    first commit

---
+++ ejercicio 1/main.js
@@ -0,0 +1,68 @@
+function contarLetras (oracion, letra) {
+    let contador = 0;
+
+
+
+
+letra= letra.toLowerCase()
+
+for (let i = 0; i < oracion.length; i++) {
+
+
+    let caracter = oracion[i].toLowerCase();
+
+    if (caracter === letra) {
+
+        contador++;
+    }
+}
+
+return contador; 
+
+
+}
+
+
+// let oracion = "El Athletic es una cuadrilla de maulas"
+// let letraRepetida = "a";
+// let resultado = contarLetra(oracion, letraRepetida)
+
+//console.log('La letra "${letraRepetida}") se repite ${resultado} veces.');
+
+
+//------------------------------------------------------------------------------------------//
+
+//otra solución
+
+//function contarLetra(oracion, letra) {
+
+  //  letra = letra.toLowerCase();
+
+    //let palabras = oracion.split("");
+
+
+    //let contador = 0;
+
+   // for (let i = 0; i < palabras.length; i++) {
+
+     //   let palabra = palabras[i].toLowerCase();
+        
+       // for (let j = 0; j < palabra.length; j++) {
+     //     if (palabra[j] === letra) {
+     //     contador++;
+    //      }
+   //   }
+   // }
+
+    //    return contdor; 
+
+    //    }
+
+    //oraciones
+
+    //let oracion = "El Athletic es una cuadrilla de maulas"
+    //let letraRepetida = "e";
+    //let resultado = contarLetra(oracion, letraRepetida);
+
+        
+    //console.log('La letra "${letraBuscada}" se repite ${resultado} veces en la oración');
---
+++ ejercicio 2/main2.js
@@ -0,0 +1,25 @@
+function imprimirImpares(numeroInicio) {
+    let contador = 0;
+    let numeroActual = numeroInicio
+
+    while (contador < 50) {
+        if (numeroActual % 2 !== 0) {
+            console.log(numeroActual);
+            contador++
+        }
+        
+        numeroActual++;
+    }
+}
+
+let numeroUsuario = parseInt(prompt("Ingrese el número:"));
+
+if (!isNaN(numeroUsuario)) {
+    imprimirImpares(numeroUsuario); 
+} else {
+    console.log("No es correcto. Introduzca un valor válido");
+}
+
+
+
+//otra solucion
